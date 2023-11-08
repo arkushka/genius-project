7dccae1ab6788a1bfea163fbd4f61274196876ff
Yuliia Arkusha <arkushka@outlook.com>
2023-11-08 11:29:38 +0200

    Add new script file

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// let a = Math.floor(Math.random() * (5 - 2) + 2);
+// console.log(a);
+
+// let name = "Yuliia";
+// console.log(`Hello ${name}`);
+
+// console.log(name.indexOf("i"));
+
+let person = {
+    name: "Yuliia",
+    age: 25,
+    children: false
+}
+
+console.log(person);
